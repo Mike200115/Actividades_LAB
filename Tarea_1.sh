9a376e6efa724f121959893da90c508b627bd0f8
Miguel Juarez <mike_alejandro_juarez@outlook.com>
2020-09-28 13:56:46 -0500

    Se intetno otra ves arreglar lo de la peticion 3

--- Tarea_1.sh
+++ Tarea_1.sh
@@ -1,19 +1,15 @@
-#s!/bin/bash
-
+#!/bin/bash
 #11e080dd9098c210fb2ed90cd2552f720ee50046103f99061816faba556a0a1b
 #@/home/mike/Documentos/1.txt
-API_KEY=$1
-ARCH=$2
-LINK=$3
+API=$1
+#ARCH=$2
+#LINK=$3
 echo '\nPeticion 1'
-#curl -X POST https://www.virustotal.com/vtapi/v2/file/scan -F apikey=$API_KEY -F file=$ARCH
+#curl -X POST https://www.virustotal.com/vtapi/v2/file/scan -F apikey=$API -F file=$ARCH
 echo '\nPeticion 2'
-#curl -X POST https://www.virustotal.com/vtapi/v2/url/scan -F apikey=$API_KEY -F url=$LINK
+#curl -X POST https://www.virustotal.com/vtapi/v2/url/scan -F apikey=$API -F url=$LINK
 echo '\nPeticion 3'
-
-url='https://www.virustotal.com/vtapi/v2/file/report?apikey='
-x='Hola'
-urlfinal=${url}${x}
-
-echo $urlfinal
-#curl --request GET --url $url Amo a mi novia
+parte="&domain=google.com"
+URL="https://www.virustotal.com/vtapi/v2/domain/report?apikey=$API"7878 
+echo "$URL"
+#curl --request GET --url ${URL}
