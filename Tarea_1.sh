2944cfd7711f02d40ed3e4870641d2bf8a4d3cf6
Miguel Juarez <mike_alejandro_juarez@outlook.com>
2020-09-29 12:59:28 -0500

    Ya se pudo terminar

--- Tarea_1.sh
+++ Tarea_1.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-#11e080dd9098c210fb2ed90cd2552f720ee50046103f99061816faba556a0a1b
-#@/home/mike/Documentos/1.txt
 API=$1
-#ARCH=$2
-#LINK=$3
-echo '\nPeticion 1'
-#curl -X POST https://www.virustotal.com/vtapi/v2/file/scan -F apikey=$API -F file=$ARCH
-echo '\nPeticion 2'
-#curl -X POST https://www.virustotal.com/vtapi/v2/url/scan -F apikey=$API -F url=$LINK
-echo '\nPeticion 3'
-parte="&domain=google.com"
-URL="https://www.virustotal.com/vtapi/v2/domain/report?apikey=$API"7878 
-echo "$URL"
-#curl --request GET --url ${URL}
+ARCH=$2
+LINK=$3
+COMMENT=$4
+echo -e "\n\n"
+echo -e "\nPeticion 1"
+curl -X POST https://www.virustotal.com/vtapi/v2/file/scan -F apikey=$API -F file=$ARCH #Example of file :@/home/mike/Doc/1.txt 
+echo -e "\nPeticion 2"
+curl -X POST https://www.virustotal.com/vtapi/v2/url/scan -F apikey=$API -F url=$LINK #Example of link : https://www.youtube.com/watch?v=S0tltAWkfLA&list=RDMM4dOT1BoJFko&index=8
+echo -e "\nPeticion 3"
+curl -v --request POST https://www.virustotal.com/vtapi/v2/comments/put -F apikey=$API -F comment=$COMMENT -F resource=8ebc97e05c8e1073bda2efb6f4d00ad7e789260afa2c276f0c72740b838a0a93 #Example of comment: Hello
+echo -e "\n"
