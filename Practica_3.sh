9ff7f2408f01407fe0ee02e3892efeca4972736d
Miguel Juarez <mike_alejandro_juarez@outlook.com>
2020-10-13 01:04:52 -0500

    Se agrego parametro en practica 3

--- Practica_3.sh
+++ Practica_3.sh
@@ -4,16 +4,17 @@
 clear
 Direccion_analizar=$1
 Direccion_maliciosa=$2
+api=$3
 
 echo 'Revisando archivos...'
 for Archivo in $(ls $Direccion_analizar)
 do
     echo "Revisando $Archivo"
     sleep 5
-    curl -X POST https://www.virustotal.com/vtapi/v2/file/scan -F apikey=11e080dd9098c210fb2ed90cd2552f720ee50046103f99061816faba556a0a1b -F file=$Direccion_analizar/$Archivo >> json.txt 
+    curl -X POST https://www.virustotal.com/vtapi/v2/file/scan -F apikey=$api -F file=$Direccion_analizar/$Archivo >> json.txt 
     grep -i 'Scan request successfully queued, come back later for the report' json.txt > /dev/null 2>&1
     [ $? -eq 1 ] && {
-        echo "Amenaza detectada $Archivo"
+        echo "Amenaza detectada $ArchivoS"
         Direccion_archivo=$Direccion_analizar/$Archivo
         mv $Direccion_archivo $Direccion_maliciosa
     }
